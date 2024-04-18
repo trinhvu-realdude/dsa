d82e4c646f02887b9000bf84bb2065db643386fc
travis-vu <realdude.sf@gmail.com>
2024-04-19 00:01:31 +0700

    Hackerrank submit

--- src/com/travis/hackerrank/problem_solving/Solution.java
+++ src/com/travis/hackerrank/problem_solving/Solution.java
@@ -687,6 +687,19 @@ class Result {
         }
         return min;
     }
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        // Write your code here
+        return 0;
+    }
 }
 
 public class Solution {
@@ -728,17 +741,13 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(firstMultipleInput[0]);
-
-        int k = Integer.parseInt(firstMultipleInput[1]);
-
-        List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
                 .collect(toList());
 
-        int result = Result.hurdleRace(k, height);
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
 
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
